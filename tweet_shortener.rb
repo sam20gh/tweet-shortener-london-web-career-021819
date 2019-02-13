40d2fba9d8b0e0dd591c49a082a0ec43d0b127b5
sam ghoreishi <sam20gh@gmail.com>
2019-02-13 23:36:09 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -1,1 +1,24 @@
-# Write your code here.
+def word_substituter(tweet)
+	words_to_be_shortened = tweet.split.collect do |word|
+		if dictionary.keys.include?(word.downcase)
+			word = dictionary[word.downcase]
+		else
+			word
+		end
+	end
+	words_to_be_shortened.join(" ")
+end
+
+
+
+def bulk_tweet_shortener
+end
+
+
+
+def selective_tweet_shortener
+end
+
+
+def shortened_tweet_truncator
+end
