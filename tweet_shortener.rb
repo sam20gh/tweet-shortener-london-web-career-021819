6112811b23790bdc208e9194e285716307976abc
sam ghoreishi <sam20gh@gmail.com>
2019-02-13 23:41:02 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -15,14 +15,14 @@ end
 
 
 def word_substituter(tweet)
-	tweet_array = tweet.split.collect do |word|
-		if dictionary.keys.include?(word.downcase)
-			word = dictionary[word.downcase]
-		else
-			word
-		end
-	end
-	tweet_array.join(" ")
+    tweet_array = tweet.split.map do |word|
+        if dictionary.keys.include?(word.downcase)
+                word = dictionary[word.downcase]
+        else
+            word
+        end
+    end
+    puts tweet_array.join(" ")
 end
 
 
