f1f44912a5faa066c9607c2f563c019ebe5dfc7b
sam ghoreishi <sam20gh@gmail.com>
2019-02-13 23:45:49 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -27,8 +27,10 @@ end
 
 
 
-def bulk_tweet_shortener(tweet)
-  puts word_substituter
+def bulk_tweet_shortener(tweet_array)
+    tweet_array.each do |tweet|
+        puts word_substituter(tweet)
+    end
 end
 
 
